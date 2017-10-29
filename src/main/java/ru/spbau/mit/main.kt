b9e3280b4905c5c99992074c740aaf6454125f6c
Plyushchenko <plyushenko@bk.ru>
2017-10-29 20:04:08 +0300

    hw001

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,75 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.io.InputStream
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class Solver(k: Int, internal val bookTitle: CharArray) {
+    companion object {
+        fun read(inputStream: InputStream) : Solver {
+            val input = Scanner(inputStream)
+            val k = input.nextInt()
+            val bookTitle = input.next().toCharArray()
+            return Solver(k, bookTitle)
+        }
+
+        val IMPOSSIBLE = "IMPOSSIBLE".toCharArray()
+    }
+
+    private val unusedLetters  = (('a' until 'a' + k) - bookTitle.toSortedSet()).toSortedSet()
+    private val halfOfBookTitleSize = bookTitle.size / 2
+
+    fun solve(): CharArray {
+        if (!removePairedQuestionMarks())
+            return IMPOSSIBLE
+        if (!removeSingleQuestionMarks())
+            return IMPOSSIBLE
+        return bookTitle
+    }
+
+    internal fun removeSingleQuestionMarks(): Boolean {
+        var i = 0
+        while (i < halfOfBookTitleSize) {
+            val correspondingIndex = getCorrespondingIndex(i)
+            if (bookTitle[i] == '?' || bookTitle[correspondingIndex] == '?') {
+                if (bookTitle[i] == '?') {
+                    bookTitle[i] = bookTitle[correspondingIndex]
+                } else {
+                    bookTitle[correspondingIndex] = bookTitle[i]
+                }
+            } else {
+                if (bookTitle[i] != bookTitle[correspondingIndex]) {
+                    return false
+                }
+            }
+            i++
+        }
+        return true
+    }
+
+    internal fun removePairedQuestionMarks(): Boolean {
+        var i = halfOfBookTitleSize
+        while (i < bookTitle.size) {
+            val correspondingIndex = getCorrespondingIndex(i)
+            if (bookTitle[i] == '?' && bookTitle[correspondingIndex] == '?') {
+                bookTitle[i] = getAndExtractMaxUnusedLetterOrA()
+                bookTitle[correspondingIndex] = bookTitle[i]
+            }
+            i++
+        }
+        return unusedLetters.isEmpty()
+    }
+
+    private fun getAndExtractMaxUnusedLetterOrA(): Char {
+        if (unusedLetters.isEmpty())
+            return 'a'
+        val lastFromUnusedLetters = unusedLetters.last()
+        unusedLetters.remove(lastFromUnusedLetters)
+        return lastFromUnusedLetters
+    }
+
+    private fun getCorrespondingIndex(i: Int): Int = bookTitle.size - i - 1
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    print(Solver.read(System.`in`).solve())
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,35 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
+import org.junit.Assert
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSample1() {
+        val solver = Solver(3, "a?c".toCharArray())
+        assertTrue(solver.removePairedQuestionMarks())
+        assertFalse(solver.removeSingleQuestionMarks())
+        Assert.assertArrayEquals(solver.solve(), "IMPOSSIBLE".toCharArray())
     }
+
+    @Test
+    fun testSample2() {
+        val solver = Solver(2, "a??a".toCharArray())
+        assertTrue(solver.removePairedQuestionMarks())
+        Assert.assertArrayEquals("abba".toCharArray(), solver.bookTitle)
+        assertTrue(solver.removeSingleQuestionMarks())
+        Assert.assertArrayEquals(solver.solve(), "abba".toCharArray())
+    }
+
+
+    @Test
+    fun testSample3() {
+        val solver = Solver(2, "?b?a".toCharArray())
+        assertTrue(solver.removePairedQuestionMarks())
+        Assert.assertArrayEquals("?b?a".toCharArray(), solver.bookTitle)
+        assertTrue(solver.removeSingleQuestionMarks())
+        Assert.assertArrayEquals(solver.solve(), "abba".toCharArray())
+    }
+
 }
